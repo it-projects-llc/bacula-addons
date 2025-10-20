a60db384d08e7f4e855d430d6faefd941f7dd2a7
Eugene Molotov <52414052+em230418@users.noreply.github.com>
2025-10-20 10:57:33 +0500

    [ADD] bacula_base_automation: миграция и перенос из приватного репозитория (#11)

---
+++ bacula_base_automation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": """Automation Rules customizations for Bacula Systems""",
+    "version": "19.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/bacula-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "base_automation",
+    ],
+    "data": [],
+    "demo": [],
+}
